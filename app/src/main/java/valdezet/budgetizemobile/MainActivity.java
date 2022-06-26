2647b4ed23813255f850dfcd1baaa230789765d9
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-06-26 19:50:54 +0800

    bottom navigation

--- app/src/main/java/valdezet/budgetizemobile/MainActivity.java
+++ app/src/main/java/valdezet/budgetizemobile/MainActivity.java
@@ -1,14 +1,35 @@
 package valdezet.budgetizemobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setupBottomNavigation();
+    }
+
+    protected void setupBottomNavigation() {
+        NavHostFragment appNavHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navhost_main_app);
+        // remove warning from IntelliJ IDE.
+        // I think during normal operation, `getNavController` won't ever return null
+        assert appNavHost != null;
+        NavController appNavController = appNavHost.getNavController();
+        BottomNavigationView appBottomNav = findViewById(R.id.bottomnav_main_app);
+        NavigationUI.setupWithNavController(appBottomNav, appNavController);
     }
 }
---
+++ app/src/main/java/valdezet/budgetizemobile/expenses/ui/ExpensesFragment.java
@@ -0,0 +1,32 @@
+package valdezet.budgetizemobile.expenses.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import valdezet.budgetizemobile.R;
+
+public class ExpensesFragment extends Fragment {
+
+
+    public ExpensesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expenses, container, false);
+    }
+}
